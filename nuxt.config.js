59269a9dc5340615a163e4e940b6461020dad1c9
Aniket Kale <aniket@geospoc.com>
2021-10-29 19:11:20 +0530

    fix: update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "first-nuxt-project",
+    title: "v-page issue",
     htmlAttrs: {
       lang: "en",
     },
